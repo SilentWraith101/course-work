bacc3989bafbafdafed2599e733ef97f8cc8f894
Jacob Mason <71287338+SilentWraith101@users.noreply.github.com>
2020-09-28 20:41:35 +0100

    fix(lesson 03): multiplication by addition.py

--- lesson 03/multiplication by addition.py
+++ lesson 03/multiplication by addition.py
@@ -1,9 +1,15 @@
+# Asking the user for a value to assign to the variables
 number = int(input('Enter the number you want to be multiplied: '))
 count = int(input('Enter the number you want to be multiplied: '))
+# Making total equal 0
 total = 0
-for i in range (count):
-    total = number + count
-    
-print('Final result =',total)
-     
-# Needs fixing
+
+# For loop to make the value times by the 2nd variable with addition
+for i in range(count):
+    total += number
+# i = 0
+# while i < count:
+#     i += 1
+#     total += number
+
+print('Final result =', total)
