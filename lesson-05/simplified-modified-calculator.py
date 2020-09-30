e0269c7ab79eb67cbd78dd2a60cc8b175a143825
Jacob Mason <71287338+SilentWraith101@users.noreply.github.com>
2020-09-30 20:41:25 +0100

    feature(calculator): added running total calculations

--- lesson-05/simplified-modified-calculator.py
+++ lesson-05/simplified-modified-calculator.py
@@ -1,3 +1,6 @@
+# It will return the result of a calculation based on an operator number
+# 1 - 4 which correspond to addition, subtraction, multiplication and
+# division.
 def calculate(operation, number1, number2):
     if operation == 1:
         return number1 + number2
@@ -9,19 +12,36 @@ def calculate(operation, number1, number2):
         return number1 / number2
 
 
+runningTotal = False
+
 while True:
 
-    # Outputting the option you can choose
-    print('Option 1: Addition, Option 2: Subtraction, Option 3: Multiplication, Option 4: Division, Option 5: Quit')
+    # Outputting the option's you can choose
+    print('''Options
+    1: Addition
+    2: Subtraction
+    3: Multiplication
+    4: Division
+    5: Quit''')
 
     option = int(input('What option do you want to use? '))
 
     if option >= 1 and option <= 4:
-        print(calculate(
+        total = calculate(
             option,
-            float(input('Enter number 1: ')),
+            runningTotal if runningTotal else float(input('Enter number 1: ')),
             float(input('Enter number 2: '))
-        ))
+        )
+        print(f'Total: {total}')
+
+        # Answer variable to see wether they want to save thier total
+        answer = str(input('Do you want to continue your calculation y/n?'))
+
+        if answer.lower() == 'y':
+            runningTotal = total
+        else:
+            print('Total not saved')
+            runningTotal = False
 
     # If statement for addition
     if option == 5:
